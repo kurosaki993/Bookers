3bcaad7eb109f7a06cdba4fe7600fb1b9012bbc3
kurosaki993 <kumkumsann@gmail.com>
2020-11-12 02:20:03 +0000

    [finish show,edit]

--- bookers/app/controllers/books_controller.rb
+++ bookers/app/controllers/books_controller.rb
@@ -16,13 +16,21 @@ class BooksController < ApplicationController
     book.save
     redirect_to '/index'
   end
-  
+
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
-
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+ 
 
   private
 
--- bookers/config/routes.rb
+++ bookers/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :blogs
+  root to: 'books#top'
   get 'top' => 'books#top'
   get 'index' => 'books#index'
   post 'books' => 'books#create'
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'book/:id', to: 'books#show'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
 end
